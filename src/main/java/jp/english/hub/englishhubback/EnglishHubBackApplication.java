a2afc35ae9f998aea6f33f136a8b75a3bb08787a
daisuke9021 <daisuke.basketball1209@gmail.com>
2021-04-29 17:53:34 +0900

    新規作成プロジェクト作成

---
+++ src/main/java/jp/english/hub/englishhubback/EnglishHubBackApplication.java
@@ -0,0 +1,13 @@
+package jp.english.hub.englishhubback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishHubBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishHubBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/english/hub/englishhubback/EnglishHubBackApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.english.hub.englishhubback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishHubBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
